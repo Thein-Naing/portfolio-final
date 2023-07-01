b304530ca1238783ae6fd3123aff4f896325bf5f
Thein Naing <theinnaing0908@gmail.com>
2023-07-01 11:30:52 +0800

    updated Contact page and style

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
+import './Contact.css';
 
 const Contact = () => {
+  return (
+    <section>
+      <h6>Contact</h6>
+      <br />
+      <ul>
+        {/* <li>
+        <i className="fa fa-link" aria-hidden="true"></i>
+          <a href="https://www.theinnaing.com/">www.theinnaing.com</a>
+        </li> */}
+        <li>
+          <a href="https://github.com/Thein-Naing">
+            <i className="fab fa-github"></i>
+          </a>
+        </li>
+        <br />
+        <li>
+          <a href="https://www.linkedin.com/in/thein-naing-394a3b52/">
+            <i className="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+        <br />
+        <br />
+        <li>
+          <i className="fa fa-envelope" aria-hidden="true"></i>
+          <br />
+          theinnaing0908@gmail.com
+        </li>
+        <br />
+        <li>
+          <i className="fa fa-phone" aria-hidden="true"></i>
+          <br />
+          (+65) 9784 5693
+        </li>
+      </ul>
+    </section>
+  );
+};
 
-    return (
-      <section>
-        <h3>Contact</h3>
-        <ul>
-          <li>
-            <i className="fas fa-globe"></i>
-            <a href="https://www.theinnaing.com/">www.theinnaing.com</a>
-          </li>
-          <li>
-          HP: (+65) 9784 5693
-          <p>theinnaing0908@gmail.com</p>
-          <p>Github</p>
-          <p>LinkedIn</p>
-          </li>
-        </ul>
-      </section>
-    );
-  }
-
-export default Contact
+export default Contact;
