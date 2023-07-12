3e28852a22bc68efc1035e34e32c732813abe100
Thein Naing <theinnaing0908@gmail.com>
2023-07-12 08:53:39 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
       A former engineer and now motivated graduate from Le Wagon software development bootcamp. I have learned Ruby, Rails, HTML, CSS and
       JavaScript about 10 weeks in bootcamp.
       After bootcamp, I continue to learn MERN stack(mongodb, express.js, react, node.js) and now I can create MERN stack app from scratch.
-      I am looking for an oppotunity to apply these skills as an entry level software developer for meaningful projects with dedicated professional software developers.
+      I am looking for an opportunity to apply these skills as an entry level software developer for meaningful projects with dedicated professional software developers.
       </p>
       <Link to="/skills"
       style={{color: 'white', textDecoration: 'none'}}
